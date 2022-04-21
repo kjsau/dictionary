ec9184da47886d0bfec8e925c40e2a1d0419ba49
kjsau <101171603+kjsau@users.noreply.github.com>
2022-04-21 16:28:25 +0800

    added phonetics

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 export default function Synonyms(props) {
-  if (props.synonyms) {
+  if (props.synonyms && props.synonyms.length > 0) {
     return (
       <ul className="Synonyms">
         {props.synonyms.map(function (synonym, index) {
-          return <li key={index}> {synonym}</li>;
+          return <li key={index}>{synonym}</li>;
         })}
       </ul>
     );
