d45d943413f147d1a30f425df81feca64c9f0f9b
kjsau <101171603+kjsau@users.noreply.github.com>
2022-04-21 12:05:12 +0800

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary Project</header>
+      <div className="container">
+        <header className="App-header">Dictionary Project</header>
+        <Dictionary />
+      </div>
     </div>
   );
 }
